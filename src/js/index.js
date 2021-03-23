c419e9b9d3a167d4efa3f966e9b7762ff75da50a
SelinaZheng23 <selina.zheng23@sitechhs.com>
2021-03-23 16:15:19 +0000

    Help

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,58 @@
+const express = require('express');
 //npm install -g parcel-bundler
 //npm init -y
 //npm i parcel-bundler --save-dev
-console.log('connected');
+//questions array, array of objects
+const questions = [
+  //question {} should contain the following
+  //the question: "String" (remeber your comma!!!),
+  //answers Array written like so: answersArray[];
+  //answer object with answer value, answer content, right/wrong, true/false, 
+
+  //aswer is string or object with question or letter?
+  {
+    question: "",
+    answersArr: [
+      { position: "A", content: "",},
+      { position: "B", content: "",},
+      { position: "C", content: "",},
+      { position: "D", content: "",},
+    ]
+  },
+    {
+    question: "",
+    answersArr: [
+      { position: "A", content: "",},
+      { position: "B", content: "",},
+      { position: "C", content: "",},
+      { position: "D", content: "",},
+    ]
+  },
+    {
+    question: "",
+    answersArr: [
+      { position: "A", content: "",},
+      { position: "B", content: "",},
+      { position: "C", content: "",},
+      { position: "D", content: "",},
+    ]
+  },
+    {
+    question: "",
+    answersArr: [
+      { position: "A", content: "",},
+      { position: "B", content: "",},
+      { position: "C", content: "",},
+      { position: "D", content: "",},
+    ]
+  },
+    {
+    question: "",
+    answersArr: [
+      { position: "A", content: "",},
+      { position: "B", content: "",},
+      { position: "C", content: "",},
+      { position: "D", content: "",},
+    ]
+  },
+];
