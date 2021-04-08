69aff44e7e1cb4409f55cddf1f27300d24397363
SelinaZheng23 <>
2021-04-08 03:16:07 +0000

    why is event listener not working?

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,51 @@
-const express = require('express');
+
+(function(){
+  //const express = require('express');
 //npm install -g parcel-bundler
 //npm init -y
 //npm i parcel-bundler --save-dev
 //questions array, array of objects
-const questionContainerElement = document.getElementById('question-container');
-function setNextQuestion(){
 
-}
+ 
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+
+const questionContainerElement = document.getElementById('question-container');
+
+const questionElement = document.getElementById('questions');
+//variable to get the element to put in the questions
+
+const answerButtonsElement = document.getElementById('answer-buttons');
+//variable to put in a text for the answer buttons
+
+let shuffleQuestions, currentQuestionIndex ;//^will default the values to undefine which is good for now. the let is used instead of const cause it will be redefined which const wouldn't allow
+
+// startButton.addEventListener('click', startGame());
+//in the variable startButton if you click it, it would perform the action of starting the Game
+document.getElementById('start-btn').addEventListener('click', startGame());
+
+function startGame(){
+  console.log('Started');
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5); //shuffles all our questions for us, gives us a question between 1 and 0 (Math.random) and the .5 would gives us a number less than 0 or above zero 50% of the time
+  currentQuestionIndex = 0; //starting on the very first question
+  questionContainerElement.classList.remove('hide');
+  //the variable questionCOntainerElement used document search and brought in the div for question-container. Here we use JS to remove the class of hide and thus unhide the element
+  setNextQuestion();
+};
+
+function setNextQuestion(){
+  //will add a function in a function
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+};
+
+function showQuestion(question){
+  questionElement.innerText = question.question;
+};
+
 function selectAnswer(){
 
-}
+};
 const questions = [
   //question {} should contain the following
   //the question: "String" (remeber your comma!!!),
@@ -20,87 +56,102 @@ const questions = [
   {
     question: "Find x: -3 + 2x = 11",
     answersArr: [
-      { position: "A", content: "8",},
-      { position: "B", content: "-1",},
-      { position: "C", content: "7",},
-      { position: "D", content: "6",},
+      { position: "A", content: "8", correct:false,},
+      { position: "B", content: "-1", correct:false,},
+      { position: "C", content: "7",correct: true,},
+      { position: "D", content: "6",correct:false,},
     ]
   },
     {
     question: "Find x: 4x + 6 = -10",
     answersArr: [
-      { position: "A", content: "4",},
-      { position: "B", content: "2",},
-      { position: "C", content: "-4",},
-      { position: "D", content: "-2",},
+      { position: "A", content: "4",correct:false,},
+      { position: "B", content: "2",correct:false,},
+      { position: "C", content: "-4",correct: true,},
+      { position: "D", content: "-2",correct:false,},
     ]
   },
     {
     question: "Find x: x + 9 = 18 - 2x",
     answersArr: [
-      { position: "A", content: "2",},
-      { position: "B", content: "1",},
-      { position: "C", content: "3",},
-      { position: "D", content: "-2",},
+      { position: "A", content: "2",correct:false,},
+      { position: "B", content: "1",correct:false,},
+      { position: "C", content: "3",correct: true,},
+      { position: "D", content: "-2",correct:false,},
     ]
   },
     {
     question: "Find x: 2x + 6 = 4x - 2",
     answersArr: [
-      { position: "A", content: "3",},
-      { position: "B", content: "-4",},
-      { position: "C", content: "4",},
-      { position: "D", content: "1",},
+      { position: "A", content: "3",correct:false,},
+      { position: "B", content: "-4",correct:false,},
+      { position: "C", content: "4",correct: true,},
+      { position: "D", content: "1",correct:false,},
     ]
   },
     {
     question: "Find x: -x - 1 = 221 + 2x",
     answersArr: [
-      { position: "A", content: "64",},
-      { position: "B", content: "74",},
-      { position: "C", content: "-74",},
-      { position: "D", content: "-64",},
+      { position: "A", content: "64",correct:false,},
+      { position: "B", content: "74",correct:false,},
+      { position: "C", content: "-74",correct: true,},
+      { position: "D", content: "-64",correct:false,},
     ]
   },
       {
     question: "Find x: 15 + 5x = 0",
     answersArr: [
-      { position: "A", content: "0",},
-      { position: "B", content: "1",},
-      { position: "C", content: "-3",},
-      { position: "D", content: "-2",},
+      { position: "A", content: "0",correct:false,},
+      { position: "B", content: "1",correct:false,},
+      { position: "C", content: "-3",correct: true,},
+      { position: "D", content: "-2",correct:false,},
     ]
   },
+  {
       question: "Find x: 17x - 12 = 114 + 3x",
     answersArr: [
-      { position: "A", content: "8",},
-      { position: "B", content: "-8",},
-      { position: "C", content: "9",},
-      { position: "D", content: "7",},
+      { position: "A", content: "8",correct:false,},
+      { position: "B", content: "-8",correct:false,},
+      { position: "C", content: "9",correct: true,},
+      { position: "D", content: "7",correct:false,},
     ]
   },
+  {
       question: "Find x: 2x - 10 = 10 - 3x",
     answersArr: [
-      { position: "A", content: "2",},
-      { position: "B", content: "3",},
-      { position: "C", content: "4",},
-      { position: "D", content: "-3",},
+      { position: "A", content: "2",correct:false,},
+      { position: "B", content: "3",correct:false,},
+      { position: "C", content: "4",correct: true,},
+      { position: "D", content: "-3",correct:false,},
     ]
   },
+  {
       question: "Find x: 12x + 0 = 144",
     answersArr: [
-      { position: "A", content: "-12",},
-      { position: "B", content: "11",},
-      { position: "C", content: "12",},
-      { position: "D", content: "-11",},
+      { position: "A", content: "-12",correct:false,},
+      { position: "B", content: "11",correct:false,},
+      { position: "C", content: "12", correct: true,},
+      { position: "D", content: "-11",correct:false,},
     ]
   },
-      question: "Find x: -10x - 19 = 19 - 8x",
+  {
+      question: "Find x: -10x + 19 = - 8x - 19",
     answersArr: [
-      { position: "A", content: "-20",},
-      { position: "B", content: "19",},
-      { position: "C", content: "-19",},
-      { position: "D", content: "20",},
+      { position: "A", content: "-20",correct:false,},
+      { position: "B", content: "19",correct:false,},
+      { position: "C", content: "-19",correct: true,},
+      { position: "D", content: "20",correct:false,},
     ]
   },
 ];
+const score = 0;
+
+//to get whether a question is correct or incorrect we use a "for loop"
+ for(
+   const i=0;
+   i < questions.length;//to loop for as many questions are in the array (in the variable questions)
+   i++ //increment i The increment operator(i++) adds 1 to the operand and the decrement operator (i--) subtracts 1 from the operand
+ ){
+
+ };
+})();
