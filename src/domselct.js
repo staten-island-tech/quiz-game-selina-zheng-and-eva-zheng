bcf41681fe1d3c0dba374e8401637de73d2c003f
evazhengg <71397947+evazhengg@users.noreply.github.com>
2021-04-11 15:06:14 -0400

    domselectors

---
+++ src/domselct.js
@@ -0,0 +1,11 @@
+const DOMSelectors = {
+    startButton: document.querySelector("#start-btn"),
+    nextButton:document.querySelector("#next-btn"),
+    questionElement: document.querySelector("#question"),
+    option: document.querySelector("#option1"),
+    option2: document.querySelector("#option2"),
+    option3: document.querySelector("#option3"),
+    option4: document.querySelector("#option4"),
+    answerButton: document.querySelector("#answerButton"),
+
+};
